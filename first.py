fbeb1b7473c97821786eed01b96b861716b8c5e9
sergejus-bartos <sergejus.bartos@inbox.lt>
2022-03-01 10:11:51 +0200

    continuing

--- first.py
+++ first.py
@@ -1,5 +1,22 @@
-def foo(my):
-    my_new = [float(ll) for ll in my]
-    return (sum(my_new))
 
-print(foo(["1.2", "1.3"]))
+from tkinter import W
+
+
+
+my_path1 = "C:/Users/serge/projects/MoreAdvancedPython/test1.txt"
+my_path2 = "C:/Users/serge/projects/MoreAdvancedPython/test2.txt"
+
+with open(my_path1, "r") as my_file1:
+    content = my_file1.read()
+    
+with open(my_path2, "a+") as my_file2:
+     my_file2.write(content)
+     my_file2.seek(0)
+     my_file2.write(content)
+     my_file2.seek(0)
+     my_file2.write(content)
+
+    
+
+
+
